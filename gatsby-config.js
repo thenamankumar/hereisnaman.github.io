c598cd81910aa4447d8abc7890dd4c6a8626b90e
Naman Kumar <naman@outlook.in>
2018-12-23 12:08:50 +0530

    added cache links

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
     `gatsby-plugin-offline`,
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-robots-txt`,
+    `gatsby-plugin-catch-links`,
     {
       resolve: 'gatsby-source-filesystem',
       options: {
